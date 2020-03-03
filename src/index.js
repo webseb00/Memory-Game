dcb7fd924287fe9a8829c5843e4010d2e4b975ed
webseb00 <lordow91@gmail.com>
2020-03-03 14:29:05 +0100

    Delete icons

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import '../scss/style.scss';
 
-// const sayHello = (str) => {
-//     document.querySelector('.title').textContent = str;
-// }
+const boardCard = document.querySelector('.board__cards');
 
-// sayHello('Hello memo app!');
+const iconsArr = ['42-radar.svg', '47-rocket.svg', '45-astronaut.svg', '38-lander.svg', '26-sputnik.svg', '18-star.svg',
+                '42-radar.svg', '47-rocket.svg', '45-astronaut.svg', '38-lander.svg', '26-sputnik.svg', '18-star.svg',
+                '42-radar.svg', '47-rocket.svg', '45-astronaut.svg', '38-lander.svg', '26-sputnik.svg', '18-star.svg',
+                '42-radar.svg', '47-rocket.svg'];
+let element;
+
+iconsArr.forEach(el => {
+    element = `<div class="board__card">
+            <div class="board__card-side board__card--front">
+            </div>
+            <div class="board__card-side board__card--back">
+                <img class="board__icon" src="../icons/SVG/EXPANDED/${el}" onload="SVGInject(this)" />
+            </div>
+        </div>`;    
+
+        boardCard.insertAdjacentHTML('beforeend', element);
+});
