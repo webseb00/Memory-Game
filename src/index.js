105b70e7e0f1ba54ab66c3b0d4439b1fd78bbd62
webseb00 <lordow91@gmail.com>
2020-03-09 21:16:13 +0100

    Create game function

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ import '../scss/style.scss';
             moves: '',
             level: '',
             time: ''
-        }
+        },
+        firstCard: null,
+        secondCard: null
     };
 
     const DOMSelectors = {
@@ -36,9 +38,10 @@ import '../scss/style.scss';
 
     const createCards = (level) => {
         // array with names of icons
-        const arr = ['6-moon.svg', '11-black hole.svg', '10-death star.svg', '16-sputnik.svg', '22-iss.svg', 
-                    '28-shuttle.svg', '30-rocket.svg', '36-lander.svg', '40-telescope.svg', '42-radar.svg',
-                    '45-astronaut.svg', '46-blaster.svg', '48-ufo.svg', '25-module.svg', '18-star.svg'];
+        const arr = ['6-moon', '11-black hole', '10-death star', '16-sputnik', '22-iss', 
+                    '28-shuttle', '30-rocket', '36-lander', '40-telescope', '42-radar',
+                    '45-astronaut', '46-blaster', '48-ufo', '25-module', '18-star',
+                    '1-venus', '9-uranus', '14-galaxy', '21-spaceport', '29-moonwalker'];
 
         const cards = [];
         let getCards = getRandomElements(arr, level);
@@ -50,20 +53,20 @@ import '../scss/style.scss';
         cards.push(getCards);
         cards.push(getCards);
         // mix randomly cards in array
-        const shuffleCards = cards.sort((a,b) => 0.5 - Math.random()).flat();
+        const shuffleCards = cards.flat().sort(() => 0.5 - Math.random());
         // pass cards to displayCards function
         displayCards(shuffleCards);
     }
 
     const displayCards = (cards) => {
         let element;
-        cards.forEach(card => {
+        cards.forEach((card, index) => {
             element = 
-            `<div class="board__card">
+            `<div id=${card} class="board__card">
                 <div class="board__card-side board__card--front">
                 </div>
                 <div class="board__card-side board__card--back">
-                    <img class="board__icon" src="../icons/SVG/EXPANDED/${card}" onload="SVGInject(this)" />
+                    <img class="board__icon" src="../icons/SVG/EXPANDED/${card}.svg" onload="SVGInject(this)" />
                 </div>
             </div>`;
             
@@ -79,6 +82,38 @@ import '../scss/style.scss';
     const displayGameInfo = () => {
         DOMSelectors.boardName.innerText = `Player: ${data.player.name}`;
         DOMSelectors.boardLevel.innerText = `Level: ${data.player.level}`;
+
+        DOMSelectors.boardCards.addEventListener('click', (e) => game(e));
+    };
+
+    const game = (e) => {
+
+        if(e.target.parentElement.classList.contains('board__card')) {
+            if(data.firstCard === null) {
+                data.firstCard = e.target.parentElement;
+                e.target.parentElement.classList.add('rotate');
+            } else {
+                data.secondCard = e.target.parentElement;
+                e.target.parentElement.classList.add('rotate');
+                DOMSelectors.boardCards.style.pointerEvents = "none";
+                    if(data.firstCard.id !== data.secondCard.id) {
+                        setTimeout(() => {
+                            data.firstCard.classList.remove('rotate');
+                            data.secondCard.classList.remove('rotate');
+                            DOMSelectors.boardCards.style.pointerEvents = "auto";
+                            data.firstCard = null;
+                            data.secondCard = null;
+                        }, 1500);
+                    } else {
+                            data.firstCard.style.pointerEvents = "none";
+                            data.secondCard.style.pointerEvents = "none";
+                            DOMSelectors.boardCards.style.pointerEvents = "auto";
+                            data.firstCard = null;
+                            data.secondCard = null;
+                    }
+            }
+          
+        }
     };
 
     const startGame = () => {
@@ -102,6 +137,9 @@ import '../scss/style.scss';
 
     const initListeners = () => {
         DOMSelectors.startButton.addEventListener('click', startGame);
+        window.addEventListener('keydown', (e) => {
+            if(e.keyCode === 13) { startGame(); };
+        });
     };
 
     const init = () => {
